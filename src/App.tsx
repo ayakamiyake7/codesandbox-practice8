2b36cf7192576bdd85a94ecb1d069e358f2fa4f9
ayakamiyake7 <88525743+ayakamiyake7@users.noreply.github.com>
2021-12-26 16:02:11 +0000

    Add data without custom hocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,59 @@
 import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/UserProfile";
+import { useState } from "react";
+
+const user = {
+  id: 1,
+  name: "Ayaka",
+  email: "aaa@aaa.com",
+  address: "London"
+};
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.street}, ${user.address.suite}, ${user.address.city}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchUser}>Fetch</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>Data Fetch Failed. </p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
